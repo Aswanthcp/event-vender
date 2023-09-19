0785a49f3b0cd2df16d22e2d14019164638e0f71
Aswanthcp <cpaswanthpalayad@gmail.com>
2023-09-19 12:21:31 +0530

    added new baseurl

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,6 @@
-export const baseUrl = "http://127.0.0.1:8000/api/";
+// export const baseUrl = "http://127.0.0.1:8000/api/";
+
+export const baseUrl = "https://eventplanneronline.online/api/";
 
 // urls
 
